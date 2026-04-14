dfda8ea1e459da2fdd5f87c5f9607ffe694a0431
Jimmy Collins <slimjim49j@gmail.com>
2026-04-14 18:59:03 -0500

    Prep for handling streams

--- main.c
+++ main.c
@@ -9,31 +9,37 @@
 #include <stdlib.h>
 #include "jc.c"
 
-// The (optional) registration configuration for the app. This sets a name for
-// the app (used for persistent storage and for debugging). It also configures
-// the execution profile, using the default "low latency" profile.
-const QUIC_REGISTRATION_CONFIG RegConfig = { "quicsample", QUIC_EXECUTION_PROFILE_LOW_LATENCY };
+typedef struct wt_server wt_server;
+typedef struct wt_con wt_con;
+typedef struct wt_stream wt_stream;
 
+struct wt_server
+{
+ ar *Ar;
+ QUIC_API_TABLE *MsQuic;
+ HQUIC Registration;
+ HQUIC Configuration;
 
-const uint16_t UdpPort = 4567;
+ wt_con *First;
+ wt_con *Free;
+};
 
-// The default idle timeout period (1 second) used for the protocol.
-const uint64_t IdleTimeoutMs = 1000;
+struct wt_con
+{
+ wt_server *Srv;
+ 
+ wt_con *Next;
 
-// The length of buffer sent over the streams in the protocol.
-const uint32_t SendBufferLength = 100;
+ wt_stream *First;
+ wt_stream *Free;
+};
 
-QUIC_API_TABLE* MsQuic = 0;
-HQUIC Registration = 0;
-HQUIC Configuration = 0;
+struct wt_stream
+{
+ wt_con *Con;
 
-// The struct to be filled with TLS secrets
-// for debugging packet captured with e.g. Wireshark.
-QUIC_TLS_SECRETS ClientSecrets = {0};
-
-// The name of the environment variable being
-// used to get the path to the ssl key log file.
-const char* SslKeyLogEnvVar = "SSLKEYLOGFILE";
+ wt_stream *Next;
+};
 
 uint8_t
 DecodeHexChar(char c)
@@ -45,7 +51,7 @@ DecodeHexChar(char c)
 }
 
 static uint32_t
-DecodeHexBuffer(char* HexBuffer, uint32_t OutBufferLen, uint8_t* OutBuffer)
+DecodeHexBuffer(char *HexBuffer, uint32_t OutBufferLen, uint8_t *OutBuffer)
 {
  uint32_t HexBufferLen = (uint32_t)strlen(HexBuffer) / 2;
  if (HexBufferLen > OutBufferLen) {
@@ -58,75 +64,80 @@ DecodeHexBuffer(char* HexBuffer, uint32_t OutBufferLen, uint8_t* OutBuffer)
  return HexBufferLen;
 }
 
-static void
-ServerSend(HQUIC Stream)
+// // The length of buffer sent over the streams in the protocol.
+// uint32_t SendBufferLength = 100;
+// static void
+// ServerSend(HQUIC Stream)
+// {
+//  //
+//  // Allocates and builds the buffer to send over the stream.
+//  //
+//  void* SendBufferRaw = malloc(sizeof(QUIC_BUFFER) + SendBufferLength);
+//  if (SendBufferRaw == NULL) {
+//      printf("SendBuffer allocation failed!\n");
+//      MsQuic->StreamShutdown(Stream, QUIC_STREAM_SHUTDOWN_FLAG_ABORT, 0);
+//      return;
+//  }
+//  QUIC_BUFFER* SendBuffer = (QUIC_BUFFER*)SendBufferRaw;
+//  SendBuffer->Buffer = (uint8_t*)SendBufferRaw + sizeof(QUIC_BUFFER);
+//  SendBuffer->Length = SendBufferLength;
+
+//  printf("[strm][%p] Sending data...\n", Stream);
+
+//  //
+//  // Sends the buffer over the stream. Note the FIN flag is passed along with
+//  // the buffer. This indicates this is the last buffer on the stream and the
+//  // the stream is shut down (in the send direction) immediately after.
+//  //
+//  QUIC_STATUS Status;
+//  if (QUIC_FAILED(Status = MsQuic->StreamSend(Stream, SendBuffer, 1, QUIC_SEND_FLAG_FIN, SendBuffer))) {
+//      printf("StreamSend failed, 0x%x!\n", Status);
+//      free(SendBufferRaw);
+//      MsQuic->StreamShutdown(Stream, QUIC_STREAM_SHUTDOWN_FLAG_ABORT, 0);
+//  }
+// }
+
+static QUIC_STATUS
+ServerStreamCallback(HQUIC QStream, void *Ctx, QUIC_STREAM_EVENT *Event)
 {
- //
- // Allocates and builds the buffer to send over the stream.
- //
- void* SendBufferRaw = malloc(sizeof(QUIC_BUFFER) + SendBufferLength);
- if (SendBufferRaw == NULL) {
-     printf("SendBuffer allocation failed!\n");
-     MsQuic->StreamShutdown(Stream, QUIC_STREAM_SHUTDOWN_FLAG_ABORT, 0);
-     return;
- }
- QUIC_BUFFER* SendBuffer = (QUIC_BUFFER*)SendBufferRaw;
- SendBuffer->Buffer = (uint8_t*)SendBufferRaw + sizeof(QUIC_BUFFER);
- SendBuffer->Length = SendBufferLength;
+ wt_stream *Stream = (wt_stream *)Ctx;
+ QUIC_API_TABLE *MsQuic = Stream->Con->Srv->MsQuic;
 
- printf("[strm][%p] Sending data...\n", Stream);
-
- //
- // Sends the buffer over the stream. Note the FIN flag is passed along with
- // the buffer. This indicates this is the last buffer on the stream and the
- // the stream is shut down (in the send direction) immediately after.
- //
- QUIC_STATUS Status;
- if (QUIC_FAILED(Status = MsQuic->StreamSend(Stream, SendBuffer, 1, QUIC_SEND_FLAG_FIN, SendBuffer))) {
-     printf("StreamSend failed, 0x%x!\n", Status);
-     free(SendBufferRaw);
-     MsQuic->StreamShutdown(Stream, QUIC_STREAM_SHUTDOWN_FLAG_ABORT, 0);
- }
-}
-
-QUIC_STATUS
-ServerStreamCallback(HQUIC Stream, void* Context, QUIC_STREAM_EVENT* Event)
-{
  switch (Event->Type) {
  case QUIC_STREAM_EVENT_SEND_COMPLETE:
  {
   // A previous StreamSend call has completed, and the context is being
   // returned back to the app.
   free(Event->SEND_COMPLETE.ClientContext);
-  printf("[strm][%p] Data sent\n", Stream);
+  printf("[strm][%p] Data sent\n", QStream);
   break;
  }
+ // todo
  case QUIC_STREAM_EVENT_RECEIVE:
  {
-  // Data was received from the peer on the stream.
-  printf("[strm][%p] Data received\n", Stream);
+  printf("[strm][%p] Data received\n", QStream);
   break;
  }
  case QUIC_STREAM_EVENT_PEER_SEND_SHUTDOWN:
  {
   // The peer gracefully shut down its send direction of the stream.
-  printf("[strm][%p] Peer shut down\n", Stream);
+  printf("[strm][%p] Peer shut down\n", QStream);
   // ServerSend(Stream);
   break;
  }
  case QUIC_STREAM_EVENT_PEER_SEND_ABORTED:
  {
   // The peer aborted its send direction of the stream.
-  printf("[strm][%p] Peer aborted\n", Stream);
-  MsQuic->StreamShutdown(Stream, QUIC_STREAM_SHUTDOWN_FLAG_ABORT, 0);
+  printf("[strm][%p] Peer aborted\n", QStream);
+  MsQuic->StreamShutdown(QStream, QUIC_STREAM_SHUTDOWN_FLAG_ABORT, 0);
   break;
  }
  case QUIC_STREAM_EVENT_SHUTDOWN_COMPLETE:
  {
   // Both directions of the stream have been shut down and MsQuic is done
   // with the stream. It can now be safely cleaned up.
-  printf("[strm][%p] All done\n", Stream);
-  MsQuic->StreamClose(Stream);
+  printf("[strm][%p] All done\n", QStream);
+  MsQuic->StreamClose(QStream);
   break;
  }
  default:
@@ -138,8 +149,11 @@ ServerStreamCallback(HQUIC Stream, void* Context, QUIC_STREAM_EVENT* Event)
 }
 
 static QUIC_STATUS
-ServerConnectionCallback(HQUIC Connection, void* Context, QUIC_CONNECTION_EVENT* Event)
+ServerConnectionCallback(HQUIC Connection, void *Ctx, QUIC_CONNECTION_EVENT *Event)
 {
+ wt_con *Con = (wt_con *)Ctx;
+ QUIC_API_TABLE *MsQuic = Con->Srv->MsQuic;
+
  switch (Event->Type)
  {
  case QUIC_CONNECTION_EVENT_CONNECTED:
@@ -178,12 +192,15 @@ ServerConnectionCallback(HQUIC Connection, void* Context, QUIC_CONNECTION_EVENT*
   MsQuic->ConnectionClose(Connection);
   break;
  }
+ // todo
  case QUIC_CONNECTION_EVENT_PEER_STREAM_STARTED:
  {
-  // The peer has started/created a new stream. The app MUST set the
-  // callback handler before returning.
   printf("[strm][%p] Peer started\n", Event->PEER_STREAM_STARTED.Stream);
-  MsQuic->SetCallbackHandler(Event->PEER_STREAM_STARTED.Stream, (void*)ServerStreamCallback, NULL);
+
+  wt_stream *Stream = ArPush(Con->Srv->Ar, wt_stream, 1);
+  Stream->Con = Con;
+  SLLStackPush(Con->First, Stream);
+  MsQuic->SetCallbackHandler(Event->PEER_STREAM_STARTED.Stream, (void *)ServerStreamCallback, (void *)Stream);
   break;
  }
  case QUIC_CONNECTION_EVENT_RESUMED:
@@ -202,18 +219,19 @@ ServerConnectionCallback(HQUIC Connection, void* Context, QUIC_CONNECTION_EVENT*
 }
 
 static QUIC_STATUS
-ServerListenerCallback(HQUIC Listener, void* Context, QUIC_LISTENER_EVENT* Event)
+ServerListenerCallback(HQUIC Listener, void *Ctx, QUIC_LISTENER_EVENT *Event)
 {
+ wt_server *Srv = (wt_server *)Ctx;
  QUIC_STATUS Status = QUIC_STATUS_NOT_SUPPORTED;
  switch (Event->Type)
  {
  case QUIC_LISTENER_EVENT_NEW_CONNECTION:
  {
-  // A new connection is being attempted by a client. For the handshake to
-  // proceed, the server must provide a configuration for QUIC to use. The
-  // app MUST set the callback handler before returning.
-  MsQuic->SetCallbackHandler(Event->NEW_CONNECTION.Connection, (void*)ServerConnectionCallback, NULL);
-  Status = MsQuic->ConnectionSetConfiguration(Event->NEW_CONNECTION.Connection, Configuration);
+  wt_con *Con = ArPush(Srv->Ar, wt_con, 1);
+  Con->Srv = Srv;
+  SLLStackPush(Srv->First, Con);
+  Srv->MsQuic->SetCallbackHandler(Event->NEW_CONNECTION.Connection, (void *)ServerConnectionCallback, (void *)Con);
+  Status = Srv->MsQuic->ConnectionSetConfiguration(Event->NEW_CONNECTION.Connection, Srv->Configuration);
   break;
  }
  case QUIC_LISTENER_EVENT_STOP_COMPLETE:
@@ -231,57 +249,73 @@ ServerListenerCallback(HQUIC Listener, void* Context, QUIC_LISTENER_EVENT* Event
 }
 
 int
-main(int argc,  char* argv[])
+main(int argc, char* argv[])
 {
+ ar *Ar = ArAlloc();
+ wt_server *Srv = ArPush(Ar, wt_server, 1);
+ Srv->Ar = Ar;
+
+ uint16_t UdpPort = 4567;
  QUIC_STATUS Status = QUIC_STATUS_SUCCESS;
- if (QUIC_FAILED(Status = MsQuicOpen2(&MsQuic))) {
+ if (QUIC_FAILED(Status = MsQuicOpen2(&Srv->MsQuic))) {
   printf("MsQuicOpen2 failed, 0x%x!\n", Status);
  }
 
- if (QUIC_FAILED(Status = MsQuic->RegistrationOpen(&RegConfig, &Registration))) {
+ const QUIC_REGISTRATION_CONFIG RegConfig = { "quicsample", QUIC_EXECUTION_PROFILE_LOW_LATENCY };
+ if (QUIC_FAILED(Status = Srv->MsQuic->RegistrationOpen(&RegConfig, &Srv->Registration))) {
   printf("RegistrationOpen failed, 0x%x!\n", Status);
  }
 
- if (MsQuic && Registration)
+ if (Srv->MsQuic && Srv->Registration)
  {
+  uint64_t IdleTimeoutMs = 1000;
   QUIC_SETTINGS Settings = {0};
   Settings.IdleTimeoutMs = IdleTimeoutMs;
   Settings.IsSet.IdleTimeoutMs = TRUE;
+  Settings.HandshakeIdleTimeoutMs = IdleTimeoutMs;
+  Settings.IsSet.HandshakeIdleTimeoutMs = TRUE;
   Settings.ServerResumptionLevel = QUIC_SERVER_RESUME_AND_ZERORTT;
   Settings.IsSet.ServerResumptionLevel = TRUE;
-  Settings.PeerBidiStreamCount = 1;
+  Settings.DatagramReceiveEnabled = TRUE;
+  Settings.IsSet.DatagramReceiveEnabled = TRUE;
+  Settings.PeerBidiStreamCount = 1000;
   Settings.IsSet.PeerBidiStreamCount = TRUE;
+  Settings.PeerUnidiStreamCount = 20;
+  Settings.IsSet.PeerUnidiStreamCount = TRUE;
+
+  Settings.StreamRecvWindowDefault = 64 * 1024;
+  Settings.IsSet.StreamRecvWindowDefault = TRUE;
+  Settings.ConnFlowControlWindow = 1024 * 1024;
+  Settings.IsSet.ConnFlowControlWindow = TRUE;
 
   QUIC_CERTIFICATE_HASH Thumbprint = {0};
-  DecodeHexBuffer("9AB69520A0BBFCA8EC5B5CA61DE4182A9008F3DD", sizeof(Thumbprint.ShaHash), Thumbprint.ShaHash);
+  DecodeHexBuffer("4f56656a0f49cd4564f0cad2766ffef89e8e4933", sizeof(Thumbprint.ShaHash), Thumbprint.ShaHash);
   QUIC_CREDENTIAL_CONFIG CredConfig = {0};
   CredConfig.Type = QUIC_CREDENTIAL_TYPE_CERTIFICATE_HASH;
   CredConfig.CertificateHash = &Thumbprint;
 
   a8 AlpnStr = CStr("h3");
   QUIC_BUFFER Alpn = { (uint32_t)AlpnStr.Ln, (uint8_t*)AlpnStr.Mem };
-  if (QUIC_FAILED(Status = MsQuic->ConfigurationOpen(Registration, &Alpn, 1, &Settings, sizeof(Settings), NULL, &Configuration)))
+  if (QUIC_FAILED(Status = Srv->MsQuic->ConfigurationOpen(Srv->Registration, &Alpn, 1, &Settings, sizeof(Settings), NULL, &Srv->Configuration)))
   {
    printf("ConfigurationOpen failed, 0x%x!\n", Status);
   }
 
-  if (QUIC_FAILED(Status = MsQuic->ConfigurationLoadCredential(Configuration, &CredConfig)))
+  if (QUIC_FAILED(Status = Srv->MsQuic->ConfigurationLoadCredential(Srv->Configuration, &CredConfig)))
   {
    printf("ConfigurationLoadCredential failed, 0x%x!\n", Status);
   }
 
   HQUIC Listener = 0;
-
   QUIC_ADDR Address = {0};
   QuicAddrSetFamily(&Address, QUIC_ADDRESS_FAMILY_UNSPEC);
   QuicAddrSetPort(&Address, UdpPort);
-
-  if (QUIC_FAILED(Status = MsQuic->ListenerOpen(Registration, ServerListenerCallback, NULL, &Listener)))
+  if (QUIC_FAILED(Status = Srv->MsQuic->ListenerOpen(Srv->Registration, ServerListenerCallback, Srv, &Listener)))
   {
    printf("ListenerOpen failed, 0x%x!\n", Status);
   }
 
-  if (QUIC_FAILED(Status = MsQuic->ListenerStart(Listener, &Alpn, 1, &Address)))
+  if (QUIC_FAILED(Status = Srv->MsQuic->ListenerStart(Listener, &Alpn, 1, &Address)))
   {
    printf("ListenerStart failed, 0x%x!\n", Status);
   }
@@ -291,18 +325,18 @@ main(int argc,  char* argv[])
 
   if (Listener)
   {
-   MsQuic->ListenerClose(Listener);
+   Srv->MsQuic->ListenerClose(Listener);
   }
  }
 
- if (MsQuic) {
-  if (Configuration) {
-   MsQuic->ConfigurationClose(Configuration);
+ if (Srv->MsQuic) {
+  if (Srv->Configuration) {
+   Srv->MsQuic->ConfigurationClose(Srv->Configuration);
   }
-  if (Registration) {
-   MsQuic->RegistrationClose(Registration);
+  if (Srv->Registration) {
+   Srv->MsQuic->RegistrationClose(Srv->Registration);
   }
-  MsQuicClose(MsQuic);
+  MsQuicClose(Srv->MsQuic);
  }
 
  return (int)Status;
