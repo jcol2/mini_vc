759109476d8e06b5efc8416323a5dfb2d9600e3b
Jimmy Collins <slimjim49j@gmail.com>
2026-05-05 07:57:18 -0500

    Remove source maps from fe build

--- build_fe.bat
+++ build_fe.bat
@@ -13,8 +13,8 @@ for /f "usebackq delims=" %%A in ("%~dp0\certs\fingerprint.hex") do (
 @REM esbuild main.ts --bundle --minify --outdir=.  --log-level=warning --sourcemap=inline
 
 @REM debug build
-esbuild main_worker.ts --bundle --outfile=main_worker.txt  --log-level=warning --sourcemap=inline --define:CertFingerprint=\"%cert_fingerprint%\"
-esbuild main.ts --bundle --outdir=.  --log-level=warning --sourcemap=inline
+esbuild main_worker.ts --bundle --outfile=main_worker.txt  --log-level=warning --define:CertFingerprint=\"%cert_fingerprint%\"
+esbuild main.ts --bundle --outdir=.  --log-level=warning
 cl /nologo /P /EP /C index_template.html /Fiindex.html
 
 endlocal
