764b2cf29bf69c48b81209b8349597cc3ea9edd7
Jimmy Collins <slimjim49j@gmail.com>
2026-05-12 12:58:27 -0500

    Add webgpu for rendering video stream

--- worker_recv.ts
+++ worker_recv.ts
@@ -1,3 +1,5 @@
+import { mat4 } from "./util";
+
 declare const CertFingerprint: string;
 interface jitter_buf_el
 {
@@ -6,17 +8,27 @@ interface jitter_buf_el
  timestamp: number,
  metadata: number,
 };
+const metadataFlagIsKey = 0x1;
 
 let wt: WebTransport;
 let decoder: VideoDecoder;
+
 let canvas: OffscreenCanvas;
-let ctx: OffscreenCanvasRenderingContext2D;
+let ctx: GPUCanvasContext;
+let device: GPUDevice;
+let pipeline: GPURenderPipeline;
+let sampler: GPUSampler;
+let uniBuf: GPUBuffer;
+let uniVals: Float32Array<ArrayBuffer>;
+let matrix: Float32Array<ArrayBuffer>;
+let renderPassDescriptor: GPURenderPassDescriptor;
 
 const jitterBufLn = 256;
 const jitterBufMask = jitterBufLn - 1;
 const jitterBuf: Array<jitter_buf_el> = new Array(jitterBufLn).fill(null).map(() => ({ frame: null, timestamp: 0, frameId: 0, metadata: 0 }));
 let jitterBufPlayIdx = 0;
 let jitterBufPlayIdxInit = 0;
+let jitterBufGotFirstFrame = 0;
 
 async function 
 WebTransportAlloc(): Promise<WebTransport>
@@ -92,7 +104,7 @@ UnidiCb(receiveStream: any)
  jitterBufEl.frame = new EncodedVideoChunk({
   data: new DataView(catBuf.buffer, frameHeaderLn), 
   timestamp: timestampMs, 
-  type: metadata ? "key" : "delta",
+  type: metadata & metadataFlagIsKey ? "key" : "delta",
   // todo specify duration
  });
  
@@ -107,7 +119,17 @@ UnidiCb(receiveStream: any)
   const decodeChunk = jitterBuf[jitterBufPlayIdx];
   if (decodeChunk.frame)
   {
-   decoder.decode(decodeChunk.frame);
+   // first frame decoded must be a keyframe
+   if (jitterBufGotFirstFrame || (decodeChunk.metadata & metadataFlagIsKey))
+   {
+    jitterBufGotFirstFrame = 1;
+    // console.log("[recv] queuing decode");
+    decoder.decode(decodeChunk.frame);
+   }
+   else
+   {
+    // console.log("[recv] nonkeyframe?", decodeChunk);
+   }
    jitterBufPlayIdx++;
    jitterBufPlayIdx %= jitterBufMask;
   }
@@ -122,9 +144,63 @@ UnidiCb(receiveStream: any)
 function
 DecoderCb(videoFrame: VideoFrame)
 {
- canvas.width = videoFrame.displayWidth;
- canvas.height = videoFrame.displayHeight;
- ctx.drawImage(videoFrame, 0, 0);
+ const width = videoFrame.displayWidth;
+ const height = videoFrame.displayHeight;
+ canvas.width = width;
+ canvas.height = height;
+
+ const fov = 60 * Math.PI / 180;  // 60 degrees in radians
+ const aspect = width / height;
+ const zNear  = 1;
+ const zFar   = 2000;
+ const projectionMatrix = mat4.perspective(fov, aspect, zNear, zFar);
+
+ const cameraPosition = [0, 0, 2];
+ const up = [0, 1, 0];
+ const target = [0, 0, 0];
+ const viewMatrix = mat4.lookAt(cameraPosition, target, up);
+ const viewProjectionMatrix = mat4.multiply(projectionMatrix, viewMatrix);
+
+ // Get the current texture from the canvas context and
+ // set it as the texture to render to.
+ renderPassDescriptor.colorAttachments[0]!.view =
+     ctx.getCurrentTexture().createView();
+ const encoder = device.createCommandEncoder();
+ const pass = encoder.beginRenderPass(renderPassDescriptor);
+ pass.setPipeline(pipeline);
+ const texture = device.importExternalTexture({source: videoFrame});
+
+ const bindGroup = device.createBindGroup({
+  layout: pipeline.getBindGroupLayout(0),
+  entries: [
+   { binding: 0, resource: sampler },
+   { binding: 1, resource: texture },
+   { binding: 2, resource: uniBuf },
+  ],
+ });
+
+ const xSpacing = 0.0;
+ const ySpacing = 0.0;
+ const zDepth = 1;
+
+ const x = -.5;
+ const y = 1;
+
+ mat4.translate(viewProjectionMatrix, [x * xSpacing, y * ySpacing, -zDepth * 0.5], matrix);
+ mat4.rotateX(matrix, 0.25 * Math.PI * Math.sign(y), matrix);
+ mat4.scale(matrix, [-5, -5, 1], matrix);
+ mat4.translate(matrix, [-0.5, -0.5, 0], matrix);
+
+ // copy the values from JavaScript to the GPU
+ device.queue.writeBuffer(uniBuf, 0, uniVals);
+
+ pass.setBindGroup(0, bindGroup);
+ pass.draw(4);
+ pass.end();
+ const commandBuffer = encoder.finish();
+ device.queue.submit([commandBuffer]);
+ // console.log("submitted frame");
+
  videoFrame.close();
 }
 
@@ -138,9 +214,117 @@ async function
 WorkerMsgCb(msg: { data: { canvas: OffscreenCanvas }})
 {
  canvas = msg.data.canvas;
- ctx = canvas.getContext("2d") as OffscreenCanvasRenderingContext2D;
+ ctx = canvas.getContext("webgpu") as GPUCanvasContext;
 
- // init wt
+
+ // webgpu init
+
+
+ const adapter = await navigator.gpu?.requestAdapter();
+ device = await adapter?.requestDevice()!;
+ if (!device)
+ {
+  console.error("need a browser that supports WebGPU");
+  return;
+ }
+ const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+ ctx.configure({
+  device,
+  format: presentationFormat,
+ });
+ const module = device.createShaderModule({
+  label: "hardcoded shaders",
+  code: `
+   struct vs_out
+   {
+    @builtin(position) position: vec4f,
+    @location(0) texcoord: vec2f,
+   };
+
+   struct uniforms
+   {
+    matrix: mat4x4f,
+   };
+
+   @group(0) @binding(2) var<uniform> uni: uniforms;
+
+   @vertex fn vs(
+    @builtin(vertex_index) vertexIndex : u32
+   ) -> vs_out
+   {
+    let pos = array(
+     vec2f(0.0, 0.0),
+     vec2f(1.0, 0.0),
+     vec2f(0.0, 1.0),
+     vec2f(1.0, 1.0),
+    );
+
+    var vsOutput: vs_out;
+    let xy = pos[vertexIndex];
+    vsOutput.position = uni.matrix * vec4f(xy, 0.0, 1.0);
+    vsOutput.texcoord = xy;
+    return vsOutput;
+   }
+
+   @group(0) @binding(0) var ourSampler: sampler;
+   @group(0) @binding(1) var ourTexture: texture_external;
+
+   @fragment fn fs(fsInput: vs_out) -> @location(0) vec4f
+   {
+    return textureSampleBaseClampToEdge(
+     ourTexture,
+     ourSampler,
+     fsInput.texcoord,
+    );
+   }
+  `,
+ });
+ pipeline = device.createRenderPipeline({
+  label: "my pipeline",
+  layout: "auto",
+  vertex: {module},
+  fragment: {
+   module,
+   targets: [{ format: presentationFormat }],
+  },
+  primitive: {
+   cullMode: "back",
+   frontFace: "ccw",
+   stripIndexFormat: "uint16",
+   topology: "triangle-strip",
+   unclippedDepth: false,
+  },
+ });
+
+ sampler = device.createSampler({
+  addressModeU: "repeat",
+  addressModeV: "repeat",
+  magFilter: "linear",
+  minFilter: "linear",
+ });
+ const uniLn = 16 * 4;
+ uniBuf = device.createBuffer({
+  label: "my uni",
+  size: uniLn,
+  usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+ });
+ uniVals = new Float32Array(uniLn / 4);
+ matrix = uniVals.subarray(0, 16);
+
+ renderPassDescriptor = {
+  colorAttachments: [
+   {
+    view: null!,
+    clearValue: [0.3, 0.3, 0.3, 1.0],
+    loadOp: "clear",
+    storeOp: "store",
+   },
+  ],
+ };
+
+
+
+ // wt init
  wt = await WebTransportAlloc();
  let reader = wt.incomingUnidirectionalStreams.getReader();
 
